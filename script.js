c4247df1866f12df37219a6cbfea5d28e05795e3
Gonthalo <gonzalogalpmg@gmail.com>
2016-03-06 19:53:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 var lienzo = document.getElementById("canvas");
 var pluma = lienzo.getContext("2d");
+pluma.font = "16px Verdana";
 var carta = document.getElementById("messages");
 var tiempo = 0;
 var dt = 0.2;
@@ -10,16 +11,17 @@ var screen_height = lienzo.height;
 var lineas = true;
 var record = false;
 var engranajes = [];
-var raton = [-1, -1];
-var square_size = 30;
-var diagrams = false;
+var square_size = 24;
+var diagrams = true;
 var master_radius = 120;
+var dcha_x = screen_width - 3*square_size/2;
+var tooth_size = 12;
 
 function signo(x){
 	if (x<0){
-		return -x;
+		return -1;
 	}
-	return x;
+	return 1;
 }
 
 //Funciones auxilares de geometría
@@ -131,7 +133,7 @@ Rueda.prototype.mover = function() {
 	}
 	this.p = [engranajes[this.ancla].x, engranajes[this.ancla].y];
 	if (this.linked != -1){
-		this.phi = -this.phi*signo(engranajes[linked].phi);
+		this.phi = -this.phi*signo(engranajes[this.linked].phi);
 	}
 }
 
@@ -139,7 +141,7 @@ Rueda.prototype.mover = function() {
 
 function randcol(){
 	var lis = [parseInt(Math.random()*3 + 1)*85, parseInt(Math.random()*3 + 1)*85, parseInt(Math.random()*3 + 1)*85];
-	if (lis[0] + lis[1] + lis[2]==0){
+	if (lis[0] == lis[1] && lis[1] == lis[2] && lis[2] == lis[0]){
 		return randcol();
 	}
 	return lis;
@@ -185,29 +187,68 @@ function radial_f(x0, y0, r, n, k, r0, phi0){
 	pluma.stroke();
 }
 
-function show_sym(name){
+function eje_sym(xx, yy, text){
+	pluma.beginPath();
+	pluma.arc(xx - square_size/3.5, yy, square_size/4, 0, 2*Math.PI);
+	pluma.stroke();
+	pluma.beginPath();
+	pluma.arc(xx - square_size/3.5, yy, square_size/8, 0, 2*Math.PI);
+	pluma.fill();
+	pluma.moveTo(xx - square_size/3.5, yy + square_size/2);
+	pluma.lineTo(xx - square_size/3.5, yy - square_size/2);
+	pluma.stroke();
+	pluma.fillText(text, xx - 5 + square_size/3, yy + 5);
+}
+
+function barra_sym(xx, yy, text){
+	var ra = square_size/4;
+	var cx = xx - square_size/3;
+	var cy = yy + square_size/2;
+	pluma.beginPath();
+	pluma.moveTo(cx - ra, yy - 2*square_size/3);
+	pluma.lineTo(cx - ra, cy);
+	pluma.arcTo(cx - ra, cy + ra, cx, cy + ra, ra);
+	pluma.arcTo(cx + ra, cy + ra, cx + ra, cy, ra);
+	pluma.lineTo(cx + ra, yy - 2*square_size/3);
+	pluma.stroke();
+	pluma.beginPath();
+	pluma.arc(cx, cy, ra/2, 0, 2*Math.PI);
+	pluma.fill();
+	pluma.beginPath();
+	pluma.arc(cx, yy - square_size/3, ra/2, 0, 2*Math.PI);
+	pluma.fill();
+	pluma.fillText(text, xx - 5 + square_size/3, yy + 5);
+}
+
+function show_sym(name, colorin, index){
 	var stack = parseInt(screen_height/square_size/3);
-	var yy = engranajes.length%stack + 0.5;
-	var xx = parseInt(engranajes.length/stack) + 0.5;
+	var yy = index%stack + 0.5;
+	var xx = parseInt(index/stack) + 0.5;
 	xx = xx*3*square_size;
 	yy = yy*3*square_size;
-	pluma.fillStyle = rgbstr(randcol());//"rgb(5, 200, 100)";
+	pluma.fillStyle = colorin;//"rgb(5, 200, 100)";
 	make_square(xx, yy);
+	pluma.fillStyle = "black";
 	if (name=="rueda"){
 		radial_f(xx, yy, gear, 7, square_size/5, square_size/1.8, 0);
+		if (index < 10){
+			pluma.fillText(index, xx - 5, yy + 5);
+		} else {
+			pluma.fillText(index, xx - 10, yy + 5);
+		}
 	}
 	if (name=="eje"){
-		pluma.beginPath();
-		pluma.arc(xx, yy, square_size/4, 0, 2*Math.PI);
 		pluma.stroke();
-		pluma.beginPath();
-		pluma.arc(xx, yy, square_size/8, 0, 2*Math.PI);
-		pluma.fill();
-		pluma.moveTo(xx + square_size/2, yy);
-		pluma.lineTo(xx - square_size/2, yy);
-		pluma.stroke();
+		if (index < 10){
+			eje_sym(xx, yy, index);
+		} else {
+			eje_sym(xx - 5, yy, index);
+		}
 	}
-	if (name=="barra"){}
+	if (name=="barra"){
+		pluma.stroke();
+		barra_sym(xx, yy, index);
+	}
 }
 
 Eje.prototype.diagrama = function() {
@@ -236,14 +277,115 @@ Barra.prototype.diagrama = function() {
 
 Rueda.prototype.diagrama = function() {
 	if (this.linked == -1){
-		this.radio = -segm(this.p, [0, 0]) + 20 + master_radius;
+		this.radio = -segm(this.p, [0, 0]) + tooth_size + master_radius;
 	} else {
-		this.radio = segm(this.p, engranajes[this.linked].p) - 20 - engranajes[this.linked].radius;
+		this.radio = segm(this.p, engranajes[this.linked].p) - Math.abs(engranajes[this.linked].radio);
+		this.radio += -signo(this.radio)*tooth_size;
 	}
-	radial_f(screen_width/2 + this.p[0], screen_height/2 + this.p[1], spike, this.n, 20, this.radio, this.phi);
+	radial_f(screen_width/2 + this.p[0], screen_height/2 + this.p[1], spike, this.n, tooth_size, this.radio, this.phi);
 };
 
 //Input y construccion del mecanismo
+
+function get_id(xx, yy){
+	return parseInt(yy/square_size/3) + parseInt(screen_height/square_size/3)*parseInt(xx/square_size/3);
+}
+
+function quitar(acting){
+	console.log(acting);
+	delete engranajes[acting];
+	for (var ii=acting + 1; ii<engranajes.length; ii++){
+		var recycle = (engranajes[ii].movil == acting || engranajes[ii].fijo == acting || engranajes[ii].linked == acting || engranajes[ii].ancla == acting)
+		console.log([ii, recycle]);
+		if (!recycle){
+			if (engranajes[ii].tipo == "rueda"){
+				if (engranajes[ii].ancla > acting){
+					engranajes[ii].ancla -= 1;
+				}
+				if (engranajes[ii].linked > acting){
+					engranajes[ii].linked -= 1;
+				}
+			}
+			if (engranajes[ii].tipo == "eje"){
+				if (engranajes[ii].ancla > acting){
+					engranajes[ii].ancla -= 1;
+				}
+			}
+			if (engranajes[ii].tipo == "barra"){
+				if (engranajes[ii].fijo > acting){
+					engranajes[ii].fijo -= 1;
+				}
+				if (engranajes[ii].movil > acting){
+					engranajes[ii].movil -= 1;
+				}
+			}
+		} else {
+			quitar(ii);
+		}
+	}
+	engranajes.splice(acting, 1);
+}
+
+function cambiar(acting){
+	var color_random = engranajes[acting].color;
+	if (engranajes[acting].tipo=="rueda"){
+		var id_eje = parseInt(window.prompt("Eje de giro:", engranajes[acting].ancla));
+		if (id_eje == -1 || id_eje>=engranajes.length){
+			return;
+		}
+		var id_linked = parseInt(window.prompt("Rueda ligada:", engranajes[acting].linked));
+		var n_dientes = parseInt(window.prompt("Nº de dientes:", engranajes[acting].n));
+		if (n_dientes == -1){
+			var p = [engranajes[id_eje].x, engranajes[id_eje].y];
+			if (id_linked == -1){
+				n_dientes = parseInt(m/master_radius*(segm(p, [0, 0]) - master_radius - tooth_size));
+			} else {
+				n_dientes = parseInt(m/engranajes[id_linked].radio*(Math.abs(engranajes[id_linked].radio) - segm(p, engranajes[id_linked].p) - tooth_size));
+			}
+		}
+		engranajes[acting] = new Rueda(id_eje, n_dientes, 0, id_linked);
+	}
+	if (engranajes[acting].tipo=="eje"){
+		var predet = engranajes[acting].ancla;
+		if (predet == -1){
+			predet = "ninguno";
+		}
+		var ref = window.prompt("Objeto anclado:", predet);
+		if (ref<0 || ref>=engranajes.length){
+			return;
+		}
+		var xx = 0;
+		var yy = 0;
+		var ancla = -1;
+		var dist = 0;
+		if (ref == "ninguno"){
+			yy = parseInt(window.prompt("Coordenada X:", engranajes[acting].x));
+			xx = parseInt(window.prompt("Coordenada Y:", engranajes[acting].y));
+		} else {
+			ref = parseInt(ref);
+			ancla = ref;
+			dist = parseInt(window.prompt("Distancia o Radio:", engranajes[acting].d));
+			xx = engranajes[ref].p[0] + dist;
+			yy = engranajes[ref].p[1];
+		}
+		engranajes[acting] = new Eje(xx, yy, ancla, dist);
+	}
+	if (engranajes[acting].tipo == "barra"){
+		var e_fijo = parseInt(window.prompt("Eje fijo:", engranajes[acting].fijo));
+		var e_libre = parseInt(window.prompt("Eje libre:", engranajes[acting].movil));
+		engranajes[acting] = new Barra(e_fijo, e_libre, [0, 0]);
+	}
+	engranajes[acting].color = color_random;
+}
+
+function boton(texto, funcion){
+	return '<input type="button" onclick="' + funcion + '" value="' + texto + '">';
+}
+
+function act(index){
+	messages.innerHTML = boton("Quitar el objeto " + index, "quitar(" + index + ")");
+	messages.innerHTML += boton("Cambiar el objeto " + index, "cambiar(" + index + ")");
+}
 
 function get_click (e){
 	var x;
@@ -258,23 +400,48 @@ function get_click (e){
 	x -= lienzo.offsetLeft;
 	y -= lienzo.offsetTop;
 	console.log(x, y);
-	raton = [x, y];
-}
-
-function get_id(xx, yy){
-	return parseInt(yy/square_size/3) + parseInt(screen_height/square_size/3)*parseInt(xx/square_size/3);
+	yyy = parseInt(y/square_size - 0.5);
+	xxx = parseInt(x/square_size - 0.5);
+	if (yyy%3 != 2){
+		if (Math.abs(x - dcha_x) < square_size){
+			if (parseInt(yyy/3) == 0){
+				crear("eje");
+			}
+			if (parseInt(yyy/3) == 1){
+				crear("barra");
+			}
+			if (parseInt(yyy/3) == 2){
+				crear("rueda");
+			}
+			return;
+		}
+		if (parseInt(screen_height/square_size/3)*parseInt(xxx/3) + parseInt(yyy/3) < engranajes.length && xxx%3 != 2){
+			act(get_id(x, y));
+			return;
+		}
+	}
+	engranajes[engranajes.length] = new Eje(y - screen_height/2, x - screen_width/2, -1, 0);
+	engranajes[engranajes.length - 1].color = rgbstr(randcol());
 }
 
 function crear(nombre){
-	show_sym(nombre);
+	var color_random = rgbstr(randcol());
+	//show_sym(nombre, color_random, engranajes.length);
 	if (nombre=="rueda"){
-		var n_dientes = parseInt(window.prompt("Nº de dientes:"));
 		var id_eje = parseInt(window.prompt("Eje de giro:"));
 		if (id_eje == -1 || id_eje>=engranajes.length){
 			return;
 		}
 		var id_linked = parseInt(window.prompt("Rueda ligada:"));
-		if (id_linked == -1){};
+		var n_dientes = parseInt(window.prompt("Nº de dientes:"));
+		if (n_dientes == -1){
+			var p = [engranajes[id_eje].x, engranajes[id_eje].y];
+			if (id_linked == -1){
+				n_dientes = parseInt(m/master_radius*(segm(p, [0, 0]) - master_radius - tooth_size));
+			} else {
+				n_dientes = parseInt(m/engranajes[id_linked].radio*(Math.abs(engranajes[id_linked].radio) - segm(p, engranajes[id_linked].p) - tooth_size));
+			}
+		}
 		engranajes[engranajes.length] = new Rueda(id_eje, n_dientes, 0, id_linked);
 	}
 	if (nombre=="eje"){
@@ -287,8 +454,8 @@ function crear(nombre){
 		var ancla = -1;
 		var dist = 0;
 		if (ref == "ninguno"){
-			xx = parseInt(window.prompt("Coordenada X:"));
-			yy = parseInt(window.prompt("Coordenada Y:"));
+			yy = parseInt(window.prompt("Coordenada X:"));
+			xx = parseInt(window.prompt("Coordenada Y:"));
 		} else {
 			ref = parseInt(ref);
 			ancla = ref;
@@ -303,7 +470,13 @@ function crear(nombre){
 		var e_libre = parseInt(window.prompt("Eje libre:"));
 		engranajes[engranajes.length] = new Barra(e_fijo, e_libre, [0, 0]);
 	}
+	engranajes[engranajes.length - 1].color = color_random;
 	engranajes[engranajes.length - 1].diagrama();
+}
+
+function creacion(){
+	nombre = window.prompt("Tipo de pieza:");
+	crear(nombre);
 }
 
 var p_P = [-100, 200];
@@ -336,6 +509,9 @@ engranajes[5] = new Eje(0, 0, 4, r1);
 engranajes[6] = new Barra(5, 2, [0, 0]);
 engranajes[7] = new Eje(0, 0, 6, d1);
 
+for (var ii = 0; ii<8; ii++){
+	engranajes[ii].color = rgbstr(randcol());
+}
 
 var punto_w = mover();
 
@@ -345,7 +521,7 @@ function mover(){
 	}
 	var p_L = [engranajes[id_pluma].x, engranajes[id_pluma].y];
 	var beta = 2*Math.PI*(tiempo/m - parseInt(tiempo/m));
-	console.log(p_L);
+	//console.log(p_L);
 	return girar(p_L, -beta);
 }
 
@@ -357,12 +533,24 @@ function magia(){
 	if (diagrams){
 		borrar();
 		mover();
+		pluma.fillStyle = "black";
 		for (var ii=0; ii<engranajes.length; ii++){
+			show_sym(engranajes[ii].tipo, engranajes[ii].color, ii);
 			engranajes[ii].diagrama();
 		}
 		master_wheel.mover();
 		master_wheel.diagrama();
 		origin.diagrama();
+		pluma.fillStyle = "white";
+		for (var ii=0; ii<3; ii++){
+			make_square(dcha_x, 3*square_size*(ii + 0.5));
+			pluma.stroke();
+		}
+		pluma.fillStyle = "black";
+		eje_sym(dcha_x, 1.5*square_size, "+");
+		barra_sym(dcha_x, 4.5*square_size, "+");
+		radial_f(dcha_x, 7.5*square_size, gear, 7, square_size/5, square_size/1.8, 0);
+		pluma.fillText("+", dcha_x - 6, 7.5*square_size + 5);
 		return;
 	}
 	var punto = mover();
@@ -387,10 +575,19 @@ function borrar(){
 }
 
 function reset(){
+	pluma.beginPath();
+	pluma.stroke();
 	tiempo = 0;
+	punto_w = mover();
+	borrar();
 }
 
 function mecanismo(){
+	if (diagrams){
+		document.getElementById("boton").value = "Ver mecanismo";
+	} else {
+		document.getElementById("boton").value = "Ver dibujo";
+	}
 	diagrams = !diagrams;
 	pluma.beginPath();
 	pluma.stroke();
